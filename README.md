7e51a10230b35dc090134d1e07ddd36a2ac3e78d
Masspwnser <74734807+Masspwnser@users.noreply.github.com>
2024-08-03 21:25:38 -0400

    Stuff is happening here, doesn't work though

--- README.md
+++ README.md
@@ -1,17 +1,3 @@
-# WebPageOCStreamer
+Fork of Brady Russel's webpage -> Opencomputers program
 
-Stream a webpage to an OpenComputers screen over TCP. Uses a Java backend with Selenium.
-
-This is just a proof of concept and not optimized.
-
-You must explicitly allow localhost connections in OpenComputers config by setting the blacklist to just \[].
-
-![Example](https://github.com/bradyrussell/WebPageOCStreamer/raw/master/webpageOCstreamer.png)
-
-Video: 
-
-[![Video](https://img.youtube.com/vi/KznpJbUaLGo/0.jpg)](https://www.youtube.com/watch?v=KznpJbUaLGo)
-
-Current Release: 
-
-https://github.com/bradyrussell/WebPageOCStreamer/releases/tag/v1.0
+https://github.com/bradyrussell/WebPageOCStreamer
--- receiver.lua
+++ receiver.lua
@@ -1,16 +1,36 @@
-function bytesToInt(bytes)
-    return bit32.bor(bit32.lshift(bit32.band(string.byte(bytes, 1) , 0xFF) , 24), bit32.bor(bit32.lshift(bit32.band(string.byte(bytes, 2) , 0xFF) , 16), bit32.bor(bit32.lshift(bit32.band(string.byte(bytes, 3) , 0xFF) , 8), bit32.band(string.byte(bytes, 4) , 0xFF))));
-end
-function RGBToInt(r, g, b)
-    return bit32.bor(bit32.lshift(bit32.band(r , 0xFF) , 16), bit32.bor(bit32.lshift(bit32.band(g , 0xFF) , 8), bit32.band(b , 0xFF)));
-end
+local color = dofile("external/Color.lua")
+local screen = dofile("external/Screen.lua")
+local image = dofile("external/Image.lua")
 
+local bit32 = require("bit32")
+local string = require("string")
 local component = require "component"
 local gpu = component.gpu
 local internet = require("internet")
-local handle = internet.open("127.0.0.1", 54321)
+local handle
+
+local function bytesToInt(bytes)
+  if bytes == nil then
+    return 0
+  end
+  return bit32.bor(bit32.lshift(bit32.band(string.byte(bytes, 1) , 0xFF) , 24), bit32.bor(bit32.lshift(bit32.band(string.byte(bytes, 2) , 0xFF) , 16), bit32.bor(bit32.lshift(bit32.band(string.byte(bytes, 3) , 0xFF) , 8), bit32.band(string.byte(bytes, 4) , 0xFF))));
+end
 
 while true do
+
+  if not handle then
+    handle = internet.open("127.0.0.1", 54321)
+  end
+
+  -- Experimental image reading
+  local picture, failure = image.readPixelData(handle, 7, 160, 50)
+  if picture then
+    screen.drawImage(0, 0, picture, false)
+  else
+    print(failure)
+    os.sleep(5)
+  end
+  -- End experimental crap
 
   local frameSize = handle:read(4)
   local frameBytes = bytesToInt(frameSize)
@@ -24,9 +44,9 @@ while true do
     local b = string.byte(handle:read(1), 1)
     local g = string.byte(handle:read(1), 1)
     local r = string.byte(handle:read(1), 1)
-    ---print(x..', '..y..': '..r..', '..g..', '..b)
+    --print(x..', '..y..': '..r..', '..g..', '..b)
 
-    gpu.setForeground(RGBToInt(r,g,b))
+    gpu.setForeground(color.RGBToInteger(r,g,b))
     gpu.fill(x, y, 1, 1, "▓")
 
     x = x + 1
